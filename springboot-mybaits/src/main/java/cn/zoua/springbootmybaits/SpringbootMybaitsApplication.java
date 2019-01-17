3f37248ed1ee7a076d103e83e6bc796b730cd91a
zhangchao <1045685272@qq.com>
2019-01-17 14:04:21 +0800

    springboot-mybatis

--- springboot-mybaits/src/main/java/cn/zoua/springbootmybaits/SpringbootMybaitsApplication.java
+++ springboot-mybaits/src/main/java/cn/zoua/springbootmybaits/SpringbootMybaitsApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan(value = "cn.zoua.springbootmybatis.mapper")
+@MapperScan(value = "cn.zoua.springbootmybaits.mapper")
 @SpringBootApplication
 public class SpringbootMybaitsApplication {
 
