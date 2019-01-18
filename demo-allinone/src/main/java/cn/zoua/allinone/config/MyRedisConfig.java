e1151a022fba99d11a2f5cf0dfe60b328fc568a6
zhangchao <1045685272@qq.com>
2019-01-18 16:49:36 +0800

    springboot-allinone

--- demo-allinone/src/main/java/cn/zoua/allinone/config/MyRedisConfig.java
+++ demo-allinone/src/main/java/cn/zoua/allinone/config/MyRedisConfig.java
@@ -1,58 +1,73 @@
 package cn.zoua.allinone.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.lang.reflect.Method;
 
 /**
- * 默认使用jdk序列化保存数据，在这里配置使用json保存缓存数据
+ * 自定义redis序列化方式
  */
-//@Configuration
-public class MyRedisConfig {
+@Configuration
+public class MyRedisConfig extends CachingConfigurerSupport {
 
-//    @Bean
-//    public RedisTemplate<Object, Employee> empRedisTemplate(
-//            RedisConnectionFactory redisConnectionFactory)
-//            throws UnknownHostException {
-//        RedisTemplate<Object, Employee> template = new RedisTemplate<Object, Employee>();
-//        template.setConnectionFactory(redisConnectionFactory);
-//        Jackson2JsonRedisSerializer<Employee> ser = new Jackson2JsonRedisSerializer<Employee>(Employee.class);
-//        template.setDefaultSerializer(ser);
-//        return template;
-//    }
-//
-//    @Bean
-//    public RedisTemplate<Object, Department> deptRedisTemplate(
-//            RedisConnectionFactory redisConnectionFactory)
-//            throws UnknownHostException {
-//        RedisTemplate<Object, Department> template = new RedisTemplate<Object, Department>();
-//        template.setConnectionFactory(redisConnectionFactory);
-//        Jackson2JsonRedisSerializer<Department> ser = new Jackson2JsonRedisSerializer<Department>(Department.class);
-//        template.setDefaultSerializer(ser);
-//        return template;
-//    }
-//
-//
-//    //CacheManagerCustomizers可以来定制缓存的一些规则
-//    @Primary  //将某个缓存管理器作为默认的
-//    @Bean
-//    public RedisCacheManager employeeCacheManager(RedisTemplate<Object, Employee> empRedisTemplate) {
-//        RedisCacheManager cacheManager = new RedisCacheManager(empRedisTemplate);
-//        //key多了一个前缀
-//
-//        //使用前缀，默认会将CacheName作为key的前缀
-//        cacheManager.setUsePrefix(true);
-//
-//        return cacheManager;
-//    }
-//
-//    @Bean
-//    public RedisCacheManager deptCacheManager(RedisTemplate<Object, Department> deptRedisTemplate) {
-//        RedisCacheManager cacheManager = new RedisCacheManager(deptRedisTemplate);
-//        //key多了一个前缀
-//
-//        //使用前缀，默认会将CacheName作为key的前缀
-//        cacheManager.setUsePrefix(true);
-//
-//        return cacheManager;
-//    }
+    @Override
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
 
+    @Bean
+    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+        RedisCacheManager rcm = new RedisCacheManager(redisTemplate);
+        //设置缓存过期时间
+        rcm.setDefaultExpiration(60 * 60 * 24);//秒
+        return rcm;
+    }
 
+    /**
+     * redisTemplate 序列化使用的jdkSerializeable, 存储二进制字节码, 所以自定义序列化类
+     */
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // 使用Jackson2JsonRedisSerialize 替换默认序列化
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+        // 设置value的序列化规则和 key的序列化规则
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
 }
